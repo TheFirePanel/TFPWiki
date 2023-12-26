8438de637fcbca6e794bace849d325c124e4cfb3
Dr.Pepper <17168168+DrPepperG@users.noreply.github.com>
2023-12-25 19:19:42 -0500

    Update and rename extension-installer.sh. to extension-installer.sh

--- config/extension-installer.sh
+++ config/extension-installer.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
-cd /var/www/html # Make sure we are set in the correct wd
+dir="/var/www/html"
+cd $dir # Make sure we are set in the correct wd
 
 # All extensions to add, [GIT URL]="BRANCH"
 declare -A repos=(
+    ["https://github.com/edwardspec/mediawiki-aws-s3"]="REL1_34"
     ["https://gerrit.wikimedia.org/r/mediawiki/extensions/MobileFrontend"]="REL1_41"
     ["https://gerrit.wikimedia.org/r/mediawiki/extensions/TemplateStyles"]="REL1_41"
 )
 
 # Go into extensions directory
-cd extensions/
+cd $dir/extensions/
 for repo in "${!repos[@]}"; do
     branch=${repos[$repo]}
     
     echo "Cloning $repo (branch: $branch)"
     git clone --depth 1 --single-branch --branch $branch $repo
 done
+
+# Run composer
+cd $dir
+echo "Running composer"
+composer update
